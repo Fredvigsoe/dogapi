f5bbb13e538640df37e3b722c434be6103af69d5
fredvigsoe <fredvigsoe@gmail.com>
2023-09-13 09:57:26 +0200

    first commit

---
+++ dogapi/src/Doglist.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function DogList() {
+  const [dogs, setDogs] = useState([]);
+
+  useEffect(() => {
+    // Brug Axios til at hente data fra API'en
+    axios
+      .get('https://api.thedogapi.com/v1/images/search?limit=10')
+      .then((response) => {
+        setDogs(response.data);
+      })
+      .catch((error) => {
+        console.error('Fejl: ' + error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>10 tilfældige hunde</h1>
+      <ul>
+        {dogs.map((dog) => (
+          <li key={dog.id}>
+            <img src={dog.url} alt="Hundebillede" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default DogList;
